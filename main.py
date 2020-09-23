898c511e64cc93661460df23a6559922f51116f6
Andrew Sherrard <sherrand@bsd.k12.in.us>
2020-09-23 17:09:17 +0000

    Added lots more flowers

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ t = turtle.Turtle()
 t.speed(50)
 canvas = t.getscreen()
 canvas.bgcolor('gray')
-def move(x,y):
+mycolors = ['red','orange','peachpuff','violet','white','blue','green','yellow']
+def flower(length,x,y):
   t.penup()
   t.goto(x,y)
   t.pendown()
-mycolors = ['red','orange','peachpuff','violet','white','blue','green','yellow']
-move(-100,100)
-for c in mycolors:
-  t.pencolor(c)
-  for i in range (4):
-   t.rt(90)
-   t.forward(45)
-  t.penup()
-  t.rt(45)
-  t.pendown()
-move (0,-100)
-for i in range(4):
-  t.lt(90)
-  t.forward(45)
+  for c in mycolors:
+    t.pencolor(c)
+    for i in range (4):
+      t.rt(90)
+      t.forward(length)
+    t.penup()
+    t.rt(45)
+    t.pendown()
+x = -120
+y = 120
+for i in range(10):
+  flower(20,x,y)
+  x = x + 60
+  y = y + 60
