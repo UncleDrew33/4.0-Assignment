9f692464fecf65b01505ed50fc1c1d3a8051bf30
Andrew Sherrard <sherrand@bsd.k12.in.us>
2020-09-25 14:10:51 +0000

    save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import turtle
 t = turtle.Turtle()
-t.speed(50)
+t.speed(100)
 canvas = t.getscreen()
 canvas.bgcolor('gray')
 mycolors = ['red','orange','peachpuff','violet','white','blue','green','yellow']
@@ -11,14 +11,14 @@ def flower(length,x,y):
   for c in mycolors:
     t.pencolor(c)
     for i in range (4):
-      t.rt(90)
+      t.rt(45)
       t.forward(length)
     t.penup()
     t.rt(45)
     t.pendown()
-x = -120
-y = 120
+x = -50
+y = 50
 for i in range(10):
   flower(20,x,y)
-  x = x + 60
-  y = y + 60
+  x = x + 30
+  y = y + 30
